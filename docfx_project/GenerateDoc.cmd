b7b2d9531096ff54760f4f015d9da30d3411bd0f
Morten Nielsen <1378165+dotMorten@users.noreply.github.com>
2019-10-14 09:10:37 -0700

    Fix comment

--- docfx_project/GenerateDoc.cmd
+++ docfx_project/GenerateDoc.cmd
@@ -1,7 +1,7 @@
 powershell -ExecutionPolicy ByPass -command "./Provision.ps1 -Version 100.6.0"
 REM Generate metadata:
 ..\.tools\docfx\docfx.exe metadata 
-REM Generate metadata:
+REM Merge metadata:
 ..\.tools\docfx\docfx.exe merge
 REM Build documentation
 REM ..\.tools\docfx\docfx.exe build
